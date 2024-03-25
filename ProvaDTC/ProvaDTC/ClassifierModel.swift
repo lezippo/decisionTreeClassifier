38537670d0fc6ada57376df9eff6cca9353ff267
lezippo <luigi.academy@icloud.com>
2024-03-25 15:24:27 +0100

    split on feature completed

--- ProvaDTC/ProvaDTC/ClassifierModel.swift
+++ ProvaDTC/ProvaDTC/ClassifierModel.swift
@@ -16,57 +16,37 @@ class DecisionTreeClassifier {
         let total_instances = Y.count
         var entropy = 0.0
         
+
         for label_count in labels_counts {
             
-            entropy += Double(label_count.1) / Double(total_instances) * log2(Double(1 / (label_count.1 / total_instances)))
+            entropy += Double(label_count.1) / Double(total_instances) * log2(1 / ((Double(label_count.1) / Double(total_instances))))
         }
-        
         
         return entropy
     }
     
-    //    def split_on_feature(self, data, feat_index):
-    //
-    //        feature_values = data[:, feat_index]
-    //        unique_values = np.unique(feature_values)
-    //
-    //        split_nodes = {}
-    //        weighted_entropy = 0
-    //        total_instances = len(data)
-    //
-    //        for unique_value in unique_values:
-    //            partition = data[data[:, feat_index] == unique_value, :]
-    //            node = Node(data=partition)
-    //            split_nodes[unique_value] = node
-    //            partition_y = self.get_y(partition)
-    //            node_entropy = self.calculate_entropy(partition_y)
-    //            weighted_entropy += (len(partition) / total_instances) * node_entropy
-    //
-    //        return split_nodes, weighted_entropy
-    
-    func split_on_feature (data: [[Double]], feat_index: Int) {
+    func split_on_feature (data: [[Double]], feat_index: Int) -> ([Double : Node], Double) {
         
-        var feature_values = data.map { $0[feat_index] }
-        var unique_values = feature_values.unique()
+        let feature_values = data.map { $0[feat_index] }
+        let unique_values = feature_values.unique()
         
-        var split_nodes = [Double: Node]()
-        var weighted_entropy = 0
+        var split_nodes = [Double: Node]() //dizionario di key double e dato Node
+        var weighted_entropy = 0.0
         var total_instances = data.count
         var node = Node()
         var node_entropy = 0.0
-        
+        var partition_y = [Double] ()
         var partition = [[Double]]()
         
         for (unique_value, _) in unique_values {
+            
             partition = data.filter { ($0[feat_index] ) == unique_value }
             node = Node(data: partition)
             split_nodes[unique_value] = node
+            partition_y = get_y(data: partition)
             
-            var partition_y = get_y(data: partition)
             node_entropy = calculate_entropy(Y: partition_y)
-            
-            print(partition)
-            print("-----------------\n")
+            weighted_entropy += (Double(partition.count) / Double(total_instances)) * node_entropy
             
             partition.removeAll()
             
@@ -74,7 +54,7 @@ class DecisionTreeClassifier {
         
         print(split_nodes)
         
-        
+        return (split_nodes, weighted_entropy)
     }
     
     
